e22f87a9a5e24895ab51412b86432b13041a725a
MaxZhubya <mazim.zh2001@gmail.com>
2020-05-31 01:00:24 +0300

    A lot of things

--- src/main/java/univ/max/kursova/view/AreaView.java
+++ src/main/java/univ/max/kursova/view/AreaView.java
@@ -1,4 +1,113 @@
 package univ.max.kursova.view;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonView;
+import univ.max.kursova.dto.AreaDTO;
+import univ.max.kursova.dto.TeamOfAreaBossDTO;
+import univ.max.kursova.model.Area;
+import univ.max.kursova.model.Brigade;
+import univ.max.kursova.model.Product;
+import univ.max.kursova.model.Workshop;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class AreaView {
+
+    @JsonView(AreaView.class)
+    private Long idArea;
+
+    @JsonView(AreaView.class)
+    private TeamOfAreaBossDTO teamOfAreaBoss;
+
+    @JsonView(AreaView.class)
+    private List<Brigade> brigadeList;
+
+    @JsonView(AreaView.class)
+    private List<Product> productList;
+
+    @JsonView(AreaView.class)
+    private Workshop workshop;
+
+    @JsonView(AreaView.class)
+    private String definition;
+
+    @JsonView(AreaView.class)
+    private LocalDateTime dateCreated;
+
+    @JsonView(AreaView.class)
+    private LocalDateTime dateModified;
+
+
+    public Long getIdArea() {
+        return idArea;
+    }
+
+    public AreaView setIdArea(Long idArea) {
+        this.idArea = idArea;
+        return this;
+    }
+
+    public TeamOfAreaBossDTO getTeamOfAreaBoss() {
+        return teamOfAreaBoss;
+    }
+
+    public AreaView setTeamOfAreaBoss(TeamOfAreaBossDTO teamOfAreaBoss) {
+        this.teamOfAreaBoss = teamOfAreaBoss;
+        return this;
+    }
+
+    public List<Brigade> getBrigadeList() {
+        return brigadeList;
+    }
+
+    public AreaView setBrigadeList(List<Brigade> brigadeList) {
+        this.brigadeList = brigadeList;
+        return this;
+    }
+
+    public List<Product> getProductList() {
+        return productList;
+    }
+
+    public AreaView setProductList(List<Product> productList) {
+        this.productList = productList;
+        return this;
+    }
+
+    public Workshop getWorkshop() {
+        return workshop;
+    }
+
+    public AreaView setWorkshop(Workshop workshop) {
+        this.workshop = workshop;
+        return this;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public AreaView setDefinition(String definition) {
+        this.definition = definition;
+        return this;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public AreaView setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+        return this;
+    }
+
+    public LocalDateTime getDateModified() {
+        return dateModified;
+    }
+
+    public AreaView setDateModified(LocalDateTime dateModified) {
+        this.dateModified = dateModified;
+        return this;
+    }
 }
--- src/main/java/univ/max/kursova/view/TechnicalPersonalView.java
+++ src/main/java/univ/max/kursova/view/TechnicalPersonalView.java
@@ -1,4 +1,98 @@
 package univ.max.kursova.view;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
+import univ.max.kursova.dto.TeamOfAreaBossDTO;
+import univ.max.kursova.dto.TechnicalPersonalDTO;
+import univ.max.kursova.model.TeamOfAreaBoss;
+import univ.max.kursova.model.enums.TechPersonalType;
+
+import java.time.LocalDateTime;
+
 public class TechnicalPersonalView {
+
+    @JsonView(TechnicalPersonalView.class)
+    private Long idTechPersonal;
+
+    @JsonView(TeamOfAreaBossView.class)
+    private TeamOfAreaBoss teamOfAreaBoss;
+
+    @JsonView(TechnicalPersonalView.class)
+    private String personalName;
+
+    @JsonView(TechnicalPersonalView.class)
+    private TechPersonalType personalType;
+
+    @JsonView(TechnicalPersonalView.class)
+    private LocalDateTime dateCreated;
+
+    @JsonView(TechnicalPersonalView.class)
+    private LocalDateTime dateModified;
+
+    @JsonView(TechnicalPersonalView.class)
+    private String description;
+
+    public Long getIdTechPersonal() {
+        return idTechPersonal;
+    }
+
+    public TechnicalPersonalView setIdTechPersonal(Long idTechPersonal) {
+        this.idTechPersonal = idTechPersonal;
+        return this;
+    }
+
+    public TeamOfAreaBoss getTeamOfAreaBoss() {
+        return teamOfAreaBoss;
+    }
+
+    public TechnicalPersonalView setTeamOfAreaBoss(TeamOfAreaBoss teamOfAreaBoss) {
+        this.teamOfAreaBoss = teamOfAreaBoss;
+        return this;
+    }
+
+    public String getPersonalName() {
+        return personalName;
+    }
+
+    public TechnicalPersonalView setPersonalName(String personalName) {
+        this.personalName = personalName;
+        return this;
+    }
+
+    public TechPersonalType getPersonalType() {
+        return personalType;
+    }
+
+    public TechnicalPersonalView setPersonalType(TechPersonalType personalType) {
+        this.personalType = personalType;
+        return this;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public TechnicalPersonalView setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+        return this;
+    }
+
+    public LocalDateTime getDateModified() {
+        return dateModified;
+    }
+
+    public TechnicalPersonalView setDateModified(LocalDateTime dateModified) {
+        this.dateModified = dateModified;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TechnicalPersonalView setDescription(String description) {
+        this.description = description;
+        return this;
+    }
 }
